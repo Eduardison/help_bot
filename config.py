03c200fadac7efbb92c8867cbadf93298a22b2e9
Eduardison <111607019+Eduardison@users.noreply.github.com>
2024-10-31 22:25:11 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+bot_token = '1459411679:AAHZ9kNvAdwjnAYb1r-GBGxpuMoEmHE170c'
+admin_id = 573264562
+
+
+clck_url = 'https://clck.ru/--?url='
+clck_url_2 = 'https://uni.su/api/?url='
+clck_url_3 = 'https://is.gd/create.php?format=simple&url='
+clck_url_4 = 'https://v.gd/create.php?format=simple&url='
